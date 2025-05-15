95ac341141602b416fa3b45f00cb9337c35467c4
Shereef Habib <sheryf.534@gmail.com>
2025-05-15 02:40:20 +0100

    refactor: enhance Prisma client initialization with global caching and logging configuration

--- src/lib/third-party/prisma.ts
+++ src/lib/third-party/prisma.ts
@@ -1,4 +1,12 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  }).$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
