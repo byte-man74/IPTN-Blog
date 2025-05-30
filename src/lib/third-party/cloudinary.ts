7f0ce49ed81c58785c3f72d32d201311e8714cd3
Shereef Habib <sheryf.534@gmail.com>
2025-05-30 18:31:07 +0100

    fix: update SEO service to return correct URL from Cloudinary upload response and enable secure connection for Cloudinary configuration

--- src/lib/third-party/cloudinary.ts
+++ src/lib/third-party/cloudinary.ts
@@ -5,6 +5,7 @@ cloudinary.v2.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true 
 })
 
 export async function deleteFileFromCloudinary(publicId: string) {
